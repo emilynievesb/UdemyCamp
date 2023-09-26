a7633ca7ed6f59717dd105b072e23930c720070d
Emily <nemily1710@gmail.com>
2023-09-26 08:34:20 -0500

    refactor: :zap: Envoiroment varibles

--- api/utils/auth/discordStrategy.js
+++ api/utils/auth/discordStrategy.js
@@ -1,5 +1,5 @@
 import DiscordStrategy from "passport-discord";
-import { authentication } from "../config.js";
+import { authentication, credentials } from "../config.js";
 import passport from "passport";
 import session from "express-session";
 import { Users } from "../../collections/users.js";
@@ -9,11 +9,11 @@ passport.use(
     {
       clientID: authentication.client,
       clientSecret: authentication.secret,
-      callbackURL: "http://127.0.0.1:3000/auth/redirect", // Cambia esto según tu configuración
+      callbackURL: `http://${credentials.hostname}:${credentials.port}/auth/redirect`,
+      // Cambia esto según tu configuración
       scope: ["identify", "guilds", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
-      console.log(profile);
       try {
         const existingUser = new Users({
           username: profile.username,
@@ -22,7 +22,6 @@ passport.use(
           avatar: profile.avatar,
         });
         const searchUser = await existingUser.searchUser();
-        console.log(searchUser);
         if (searchUser.length === 0) {
           await existingUser.postUser();
         }
