a5f6a06fadf1a4c52900658dbf58d46039d009b5
Emily <nemily1710@gmail.com>
2023-09-30 15:12:45 -0500

    feat: :sparkles: Section footer

---
+++ src/components/SectionFooter/index.jsx
@@ -0,0 +1,33 @@
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Typography } from "@mui/material";
+function SectionFooter() {
+  return (
+    <>
+      <CssBaseline />
+      <Container
+        maxWidth="xl"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: { lg: "70vh" },
+          textAlign: "center",
+          backgroundColor: "#e5e5e5",
+        }}
+      >
+        <Typography variant="h2" component="h1">
+          "A good software developer works with discipline and consistency from
+          day one."
+          <br />
+        </Typography>
+        <Typography variant="h4" component="h1">
+          - Omar Bradley -
+          <br />
+        </Typography>
+      </Container>
+    </>
+  );
+}
+export { SectionFooter };
