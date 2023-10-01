d8a4d5c4a4b3d5f151f203191216c4847b3976e1
Emily <nemily1710@gmail.com>
2023-09-30 20:23:32 -0500

    feat: :zap: change enviroment variables

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const env = loadEnv("development", process.cwd(), "VITE");
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5116,
-    host: "127.1.1.1",
+    port: env.VITE_PORT,
+    host: env.VITE_HOST,
   },
 });
